511dc9fd54d676dc09436e220ce74684e538ec40
Brian7Carrasco <Brian.carrasco20@outlook.com>
2025-07-23 14:51:43 -0600

    7/23/25
    Fixing some designers

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,87 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 
+import LoginPage from "./components/LoginPage";
+import RegisterPage from "./components/RegisterPage";
 import DashboardPage from "./components/DashboardPage";
 import CalendarPage from "./components/CalendarPage";
 import WorkoutPage from "./components/WorkoutPage";
 import GoalsPage from "./components/GoalsPage";
 import SettingsPage from "./components/SettingsPage";
 import BottomNavBar from "./components/BottomNavBar";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+const AppContent: React.FC = () => {
+  const location = useLocation();
+  const hideNav =
+    location.pathname === "/" ||
+    location.pathname === "/login" ||
+    location.pathname === "/register";
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/calendar"
+          element={
+            <ProtectedRoute>
+              <CalendarPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/workout"
+          element={
+            <ProtectedRoute>
+              <WorkoutPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/goals"
+          element={
+            <ProtectedRoute>
+              <GoalsPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/settings"
+          element={
+            <ProtectedRoute>
+              <SettingsPage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+
+      {!hideNav && <BottomNavBar />}
+    </>
+  );
+};
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Routes>
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/calendar" element={<CalendarPage />} />
-          <Route path="/workout" element={<WorkoutPage />} />
-          <Route path="/goals" element={<GoalsPage />} />
-          <Route path="/settings" element={<SettingsPage />} />
-          <Route path="*" element={<DashboardPage />} />
-        </Routes>
-
-        <BottomNavBar />
+        <AppContent />
       </div>
     </Router>
   );
